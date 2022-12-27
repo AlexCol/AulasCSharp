0395a2172850d19e8d5fd8970d9f40c809e5ae72
AlexCol <eu_axil@yahoo.com.br>
2022-12-27 16:12:09 -0300

    Update Program.cs

--- Projeto1/Program.cs
+++ Projeto1/Program.cs
@@ -3,7 +3,7 @@
 namespace projeto1 {
     internal class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!!!!!!!!!!!!!!!!!");
+            Console.WriteLine("Hello World!");
             //Console.WriteLine("Oi");
             
             //string? linha;
