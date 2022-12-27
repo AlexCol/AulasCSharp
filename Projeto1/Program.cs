1ae9526710c0a1eeb73d34b15e2768e82c3990b6
Alexandre Coletti <eu_axil@yahoo.com.br>
2022-12-27 16:58:15 -0300

    commit via vs code

--- Projeto1/Program.cs
+++ Projeto1/Program.cs
@@ -4,7 +4,7 @@ namespace projeto1 {
     internal class Program {
         static void Main(string[] args) {
             Console.WriteLine("Hello World!");
-            //Console.WriteLine("Oi");
+            Console.WriteLine("Oi");
             
             //string? linha;
             //linha = Console.ReadLine();
