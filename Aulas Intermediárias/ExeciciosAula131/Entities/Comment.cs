9b3f5ad0a7990d239ea6d6d057020ac6eee0af29
Alexandre Coletti <eu_axil@yahoo.com.br>
2022-12-28 12:55:58 -0300

    commit

---
+++ Aulas Intermediárias/ExeciciosAula131/Entities/Comment.cs
@@ -0,0 +1,16 @@
+﻿namespace Course.Entities
+{
+    class Comment
+    {
+        public string Text { get; set; }
+
+        public Comment() {
+            Text = "";
+        }
+
+        public Comment(string text)
+        {
+            Text = text;
+        }
+    }
+}
