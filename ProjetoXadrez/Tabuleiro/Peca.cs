25bb2d1cf0a8511291fee1db6936a59e7be05dc1
Alexandre Coletti <eu_axil@yahoo.com.br>
2023-01-02 14:55:00 -0300

    commit

--- ProjetoXadrez/Tabuleiro/Peca.cs
+++ ProjetoXadrez/Tabuleiro/Peca.cs
@@ -1,14 +1,14 @@
 namespace tabuleiro;
 class Peca
 {
-    public Posicao posicao { get; set; }
+    public Posicao? posicao { get; set; }
     public Cor cor { get; set; }
     public int qtdeMovimentos { get; protected set; }
     public Tabuleiro tabuleiro { get; protected set; }
 
-    public Peca(Posicao posicao, Cor cor, Tabuleiro tabuleiro)
+    public Peca(Tabuleiro tabuleiro, Cor cor)
     {
-        this.posicao = posicao;
+        this.posicao = null;
         this.cor = cor;
         this.tabuleiro = tabuleiro;
         this.qtdeMovimentos = 0;
