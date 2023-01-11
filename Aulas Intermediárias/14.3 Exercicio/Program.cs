ad528677c3dcbd06661d20a2a9112cb06beb718c
Alexandre Coletti <eu_axil@yahoo.com.br>
2023-01-11 07:44:19 -0300

    .

---
+++ Aulas Intermediárias/14.3 Exercicio/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using Course.Entities;
+using Course.Services;
+
+namespace Course {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Enter contract data");
+            Console.Write("Number: ");
+            int contractNumber = int.Parse(new String(Console.ReadLine()));
+            Console.Write("Date (dd/MM/yyyy): ");
+            DateTime contractDate = DateTime.ParseExact(new String(Console.ReadLine()), "dd/MM/yyyy", CultureInfo.InvariantCulture);
+            Console.Write("Contract value: ");
+            double contractValue = double.Parse(new String(Console.ReadLine()), CultureInfo.InvariantCulture);
+            Console.Write("Enter number of installments: ");
+            int months = int.Parse(new String(Console.ReadLine()));
+
+            Contract myContract = new Contract(contractNumber, contractDate, contractValue);
+
+            ContractService contractService = new ContractService(new PaypalService());
+            contractService.ProcessContract(myContract, months);
+
+            Console.WriteLine("Installments:");
+            foreach (Installment installment in myContract.Installments) {
+                Console.WriteLine(installment);
+            }
+        }
+    }
+}
