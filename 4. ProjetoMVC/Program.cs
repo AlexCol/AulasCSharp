253e96d3d8ee313fd4a5890264cec000c9cc0f21
Alexandre Coletti <eu_axil@yahoo.com.br>
2023-03-20 09:51:11 -0300

    ..

--- 4. ProjetoMVC/Program.cs
+++ 4. ProjetoMVC/Program.cs
@@ -1,3 +1,4 @@
+using _4._ProjetoMVC.Data;
 using _4._ProjetoMVC.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -11,12 +12,28 @@ builder.Services.AddMySql<SalesWebMvcContext>(conectionString,
                         ServerVersion.AutoDetect(conectionString)
                     );
 
+builder.Services.AddScoped<SeedingService>();
+
 var app = builder.Build();
+
+
+//usando SeedingServide
+if (app.Environment.IsDevelopment())
+{
+    using (var serviceScope = app.Services.CreateScope())
+    {
+        var services = serviceScope.ServiceProvider;
+        var meuSeed = services.GetRequiredService<SeedingService>();
+        //Use the service
+        meuSeed.Seed();
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
+
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
